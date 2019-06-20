689c0e89b58ba8207ac5fa6ac8de4b3739f628af
gmkseta <gmkseta@gmail.com>
2019-06-20 17:51:14 +0900

    add auth

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -3,17 +3,21 @@ class DevicesController < ApplicationController
 
     def open
         
-        url = "http://15.164.75.24:3000/emart24"
-        
-        response = HTTParty.post(
-            url, 
-            headers: {
-                'Content-Type'=>'application/json'}, 
-            body: {
-                "username" => @current_user.username
-            }
-        )
-        resp = JSON.parse(response.body)
-        render json: resp
+        if @current_user.auth?
+            url = "http://15.164.75.24:3000/emart24"
+            
+            response = HTTParty.post(
+                url, 
+                headers: {
+                    'Content-Type'=>'application/json'}, 
+                body: {
+                    "username" => @current_user.username
+                }
+            )
+            resp = JSON.parse(response.body)
+            render json: resp
+        else 
+            render json: {"status": "UnAuthorized"}
+        end
     end
 end
